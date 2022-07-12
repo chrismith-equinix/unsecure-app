63ffdb8c950edce54201ce18ba445cd12a4db8b3
chrismith-equinix <chrismith@equinix.com>
2022-07-12 11:11:37 -0400

    Inital commit

--- app/controllers/cloud_controller.rb
+++ app/controllers/cloud_controller.rb
@@ -1,5 +1,11 @@
 class CloudController < ApplicationController
-  def push_file_to_s3
+  def push_file_to_s3(filename)
+    aws_access_key_id = "AKIAIOSFODNN7NOTREAL"
+    aws_secret_access_key = "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"
 
+    creds = Aws::Credentials.new(aws_access_key_id, aws_secret_access_key)
+    s3 = Aws::S3::Resource.new(region: 'us-west-1', credentials: creds)
+    obj = s3.bucket('myproject').object"/some/path/#{filename}")
+    obj.public_url
   end
 end
